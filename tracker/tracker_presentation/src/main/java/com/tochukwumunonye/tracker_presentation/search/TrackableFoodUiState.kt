abd0dad1b8165fde4088df314b19b5d38581ddf5
Tochukwumunonye <tochukwuokeymunonye@gmail.com>
2022-04-02 21:36:44 +0100

    Initial commit

---
+++ tracker/tracker_presentation/src/main/java/com/tochukwumunonye/tracker_presentation/search/TrackableFoodUiState.kt
@@ -0,0 +1,9 @@
+package com.tochukwumunonye.tracker_presentation.search
+
+import com.tochukwumunonye.tracker_domain.model.TrackableFood
+
+data class TrackableFoodUiState(
+    val food: TrackableFood,
+    val isExpanded: Boolean = false,
+    val amount: String = ""
+)
